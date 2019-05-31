8ab12a2104bc69a29bf8f1898e90ad2eaeeaaf95
Amor Osmic <amorosmic@gmx.com>
2019-05-31 15:13:50 +0200

    Wrote a test for account class

---
+++ src/toDoListPackage/AccountTest.java
@@ -0,0 +1,41 @@
+package toDoListPackage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AccountTest {
+
+	@BeforeClass
+	public static void createTestingEnviroment() {
+		
+		String userName, password, email;
+		
+		userName = new String("Anersyum");
+		password = new String("password");
+		email = new String("amorosmic@gmx.com");
+		
+		AccountManager.createAccount(userName, email, password);
+		
+		userName = "DjChupolini";
+		password = "Pas_word";
+		email = "alexilaiho467@gmail.com";
+		
+		AccountManager.createAccount(userName, email, password);
+	}
+	
+	@Test
+	public void shouldThrowErrorIfThereAreNoAccountsPresent() {
+		
+		assertEquals(2, AccountManager.getAccountList().size());
+	}
+	
+	@Test
+	public void shouldThrowErrorIfAccountIsNull() {
+		
+		Account account = AccountManager.getAccount("amorosmic@gmx.com", "password");
+		
+		assertEquals("Anersyum", account.getUserName());
+	}
+}
