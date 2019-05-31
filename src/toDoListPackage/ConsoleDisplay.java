2370bff59fdeea98ed99711796d62cea89fb9f95
Amor Osmic <amorosmic@gmx.com>
2019-05-31 15:20:30 +0200

    Added text to show for log in and account creation

--- src/toDoListPackage/ConsoleDisplay.java
+++ src/toDoListPackage/ConsoleDisplay.java
@@ -10,6 +10,18 @@ public class ConsoleDisplay {
 				+ "\n3. Show unfinshed tasks"
 				+ "\n4. Show finished tasks"
 				+ "\n5. Finish task"
+				+ "\n6. Unfinish task"
 				+ "\n0. Exit");
+	}
+	
+	public static void showLogInText() {
+		
+		System.out.println("Welcome to the To Do list app.\nIf you have an account, please input 1."
+				+ "\nIf you don't have an account and wish to create one, please input 0");
+	}
+	
+	public static void showAccountCreationText() {
+		
+		System.out.println("Here are the details we need to create you an account:");
 	}
 }
