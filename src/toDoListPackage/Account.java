94f1b2d8e805b4146ac89f30bce4e334c8968b20
Amor Osmic <amorosmic@gmx.com>
2019-05-31 10:34:22 +0200

    Added Account and AccountManager classes

---
+++ src/toDoListPackage/Account.java
@@ -0,0 +1,29 @@
+package toDoListPackage;
+
+public class Account {
+
+	private String userName, email, password;
+	
+	public Account(String userName, String email, String password) {
+		
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
---
+++ src/toDoListPackage/AccountManager.java
@@ -0,0 +1,36 @@
+package toDoListPackage;
+
+import java.util.ArrayList;
+
+public class AccountManager {
+
+	private static ArrayList<Account> listOfAccounts = new ArrayList<Account>();
+	
+	public static void createAccount(String userName, String email, String password) {
+		
+		Account newAccout = new Account(userName, email, password);
+		
+		addAccountToAccountsList(newAccout);
+		
+		System.out.println("Your account has been created!");
+	}
+	
+	private static void addAccountToAccountsList(Account account) {
+		
+		listOfAccounts.add(account);
+	}
+	
+	public static Account getAccount(String email, String password) {
+		
+		for (Account account : listOfAccounts) {
+			
+			if (account.getPassword().equals(password)) {
+				
+				return account;
+			}
+		}
+		
+		return null;
+	}
+	
+}
