8b66080d8c78817f98651a9bb4845417c5e382ce
Amor Osmic <amorosmic@gmx.com>
2019-05-31 10:38:51 +0200

    Created a log in manager.

---
+++ src/toDoListPackage/LogInManager.java
@@ -0,0 +1,27 @@
+package toDoListPackage;
+
+public class LogInManager {
+
+	public static Account logIn(String email, String password) {
+		
+		if (isValidLogin(email, password)) {
+			
+			return AccountManager.getAccount(email, password);
+		}
+		
+		return null;
+	}
+	
+	public static void logOut() {
+		
+		//code to save all changes made to the account
+	}
+	
+	private static boolean isValidLogin(String email, String password) {
+		
+		if (AccountManager.getAccount(email, password) == null)
+			return false;
+		
+		return true;
+	}
+}
