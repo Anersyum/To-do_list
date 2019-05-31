437175128ce405df26aa505eccde54cff5380604
Amor Osmic <amorosmic@gmx.com>
2019-05-31 11:51:21 +0200

    Added a validator class to validate input

---
+++ src/toDoListPackage/Validator.java
@@ -0,0 +1,38 @@
+package toDoListPackage;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Validator {
+
+	public static int checkInput(Scanner input) {
+
+		int consoleChoice = 0;
+
+		while (true) {
+
+			try {
+				consoleChoice = input.nextInt();
+
+				if (consoleChoice < 0 || consoleChoice > 5) {
+
+					System.out.println("That's not a valid input! Try again:");
+					continue;
+				}
+				else 
+					break;
+
+			} catch (InputMismatchException e) {
+
+				System.out.println("You didn't enter a number! Try again:");
+				input.nextLine();
+				continue;
+			}
+		}
+
+		return consoleChoice;
+
+	}
+
+}
+
