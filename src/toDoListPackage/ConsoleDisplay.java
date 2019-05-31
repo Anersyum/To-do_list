c1777c5bfd425148c6d0f8fea51f41617a7b9707
Amor Osmic <amorosmic@gmx.com>
2019-05-31 11:50:32 +0200

    Added a class to display options you can choose

---
+++ src/toDoListPackage/ConsoleDisplay.java
@@ -0,0 +1,15 @@
+package toDoListPackage;
+
+public class ConsoleDisplay {
+
+	public static void showConsole() {
+		
+		System.out.println("To Do list:"
+				+ "\n1. Create new to do list"
+				+ "\n2. Create new task"
+				+ "\n3. Show unfinshed tasks"
+				+ "\n4. Show finished tasks"
+				+ "\n5. Finish task"
+				+ "\n0. Exit");
+	}
+}
