72d61a7f6320052b3a481a60bd8e2dbae38fda42
Amor Osmic <amorosmic@gmx.com>
2019-06-03 12:31:35 +0200

    Added a InputValidatorTest class and refactored the InputValidator class

--- src/toDoListPackage/InputValidator.java
+++ src/toDoListPackage/InputValidator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class InputValidator {
 
-	public static int checkInput(Scanner input, int minInput, int maxInput) {
+	public static int returnInputIfInputIsInRangeAndIsTypeOfInt(Scanner input, int minInput, int maxInput) {
 
 		int consoleChoice = 0;
 
@@ -32,6 +32,56 @@ public class InputValidator {
 
 		return consoleChoice;
 
+	}
+	
+	public static String returnEmailIfEmailIsValid(Scanner input) {
+		
+		String email = new String();
+		email = input.next();
+		
+		while (!isEmailValid(email)) {
+			
+			System.out.print("That is an invalid email. Try again (the format is text@mail.domain, for example user@gmail.com)."
+					+ "\nInput: ");
+			email = input.next();
+		}
+		
+		return email;
+	}
+	
+	public static boolean isEmailValid(String email) {
+		
+		String pattern = "(\\S*)@(\\S*)(\\.)(\\S*)";
+		
+		if (email.matches(pattern))
+			return true;
+		
+		return false;
+	}
+	
+	public static String returnPasswordIfPasswordIsValid(Scanner input) {
+		
+		String password = new String();
+		password = input.next();
+		
+		while (!isPasswordValid(password)) {
+			
+			System.out.print("That is an invalid password. Try again (the password has to have at least one uppercase character and one number. Also it needs to be longer than 8 characters.)."
+					+ "\nInput: ");
+			password = input.next();
+		}
+		
+		return password;
+	}
+	
+	public static boolean isPasswordValid(String password) {
+		
+		String pattern = "^(?=.*\\d)(?=.*[A-Z]).{8,}$";
+		
+		if (password.matches(pattern) && password.length() > 8)
+			return true;
+		
+		return false;
 	}
 
 }
---
+++ src/toDoListPackage/InputValidatorTest.java
@@ -0,0 +1,38 @@
+package toDoListPackage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Scanner;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class InputValidatorTest {
+
+	static Scanner input;
+	
+	@BeforeClass
+	public static void createScanner() {
+		
+		input = new Scanner(System.in);
+	}
+	
+	@Test
+	public void shouldThrowErrorIfPasswordNotValid() {
+		
+		String password = new String();
+		
+		System.out.println("Enter password: (at least 8 characters long and contains one uppercase letter and one number)");
+		password = input.nextLine();
+		
+		assertEquals(true, InputValidator.isPasswordValid(password));
+	}
+	
+	@Test
+	public void shouldThrowErrorIfEmailNotValid() {
+		
+		String email = new String("amor@mail.com");
+		
+		assertEquals(true, InputValidator.isEmailValid(email));
+	}
+}
