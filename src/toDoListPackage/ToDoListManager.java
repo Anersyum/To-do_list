51b7696abaccff86e2e07e26e240ac9e7c894b4c
Amor Osmic <amorosmic@gmx.com>
2019-06-19 18:06:42 +0200

    Rewrote the saving accounts and files and loading them feature

--- src/toDoListPackage/ToDoListManager.java
+++ src/toDoListPackage/ToDoListManager.java
@@ -10,99 +10,120 @@ import java.util.Scanner;
 public class ToDoListManager {
 
 	private static ArrayList<ToDoList> toDoLists = new ArrayList<ToDoList>();
-	
+
 	public static void createNewToDoList(String listName) {
-		
+
 		ToDoList list = new ToDoList(listName);
-		
+
 		addToDoListToArrayList(list);
-		
+
 		System.out.println("Your list has been created!\n");
-	} 
-	
+	}
+
 	private static void addToDoListToArrayList(ToDoList list) {
-		
+
 		toDoLists.add(list);
 	}
-	
+
 	public static ToDoList getToDoList(String listName) {
-		
-		return toDoLists.stream().filter(list -> list.getListName().equals(listName))
-					.findFirst().orElse(null);
+
+		return toDoLists.stream().filter(list -> list.getListName().equals(listName)).findFirst().orElse(null);
 	}
-	
+
 	public static ArrayList<ToDoList> getToDoLists() {
-		
+
 		return toDoLists;
 	}
-	
+
 	public static void saveListToAccountExternalFile() {
-		
+
 		Account openedAccount = AccountManager.getAccount();
-		File fileToSaveListTo = new File(System.getProperty("user.dir") + "//ToDoLists//" + openedAccount.getUserName() + ".txt");
-		
+		File fileToSaveListTo = new File(
+				System.getProperty("user.dir") + "//ToDoLists//" + openedAccount.getUserName() + "//toDoList.txt");
+
+		fileToSaveListTo.getParentFile().mkdirs();
 		try (PrintWriter output = new PrintWriter(fileToSaveListTo)) {
-			
+
 			for (ToDoList list : toDoLists) {
-				
+
 				output.println(list.getListName());
-				
-				for (Task task : list.getTaskList()) {
-					
-					output.println(task.getTaskName());
-					output.println(task.getTaskDescription());
-					output.println(task.getTag());
-					output.println(task.getDueDate());
+
+				File taskFile = new File(System.getProperty("user.dir") + "//ToDoLists//" + openedAccount.getUserName()
+						+ "//" + list.getListName() + ".txt");
+
+				try (PrintWriter taskOutPut = new PrintWriter(taskFile)) {
+
+					for (Task task : list.getTaskList()) {
+
+						taskOutPut.println(task.getTaskName());
+						taskOutPut.println(task.getTaskDescription());
+						taskOutPut.println(task.getTag());
+						taskOutPut.println(task.isFinished());
+						taskOutPut.println(task.getDueDate());
+					}
 				}
 			}
-			
+
 			System.out.println("Your changes have been saved!");
-		}
-		catch (IOException e) {
-			
+		} catch (IOException e) {
+
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	//fix loading from file
+
+	// fix loading from file
 	public static void loadToDoListsFromFile() {
-		
+
 		Account openedAccount = AccountManager.getAccount();
-		File loadListFile = new File(System.getProperty("user.dir") + "//ToDoLists//" + openedAccount.getUserName() + ".txt");
-		
+		File loadListFile = new File(
+				System.getProperty("user.dir") + "//ToDoLists//" + openedAccount.getUserName() + "//toDoList.txt");
+
 		if (loadListFile.exists()) {
 
 			try (Scanner readFromFile = new Scanner(loadListFile)) {
-				
+
 				String listName = new String();
 				String taskName = new String();
-				String taskDes = new String();
+				String taskDescription = new String();
 				String taskTag = new String();
+				String isFinished = new String();
 				LocalDate dueDate;
 				String[] time;
 				int[] timeInt = new int[3];
-				
+
 				while (readFromFile.hasNext()) {
 
 					listName = readFromFile.nextLine();
-					System.out.println(listName + " ime liste");
-					
+
 					createNewToDoList(listName);
-					
+
 					ToDoList list = getToDoList(listName);
-					
-					taskName = readFromFile.nextLine();
-					taskDes = readFromFile.nextLine();
-					taskTag = readFromFile.nextLine();
-					time = readFromFile.nextLine().split("-");
-					
-					for (int i = 0; i < 3; i++) {
+					File taskFile = new File(System.getProperty("user.dir") + "//ToDoLists//"
+							+ openedAccount.getUserName() + "//" + list.getListName() + ".txt");
+
+					try (Scanner readTaskFile = new Scanner(taskFile)) {
 						
-						timeInt[i] = Integer.valueOf(time[i]);
+						while (readTaskFile.hasNext()) {
+							
+							taskName = readTaskFile.nextLine();
+							taskDescription = readTaskFile.nextLine();
+							taskTag = readTaskFile.nextLine();
+							isFinished = readTaskFile.nextLine();
+							time = readTaskFile.nextLine().split("-");
+							
+							for (int i = 0; i < 3; i++) {
+
+								timeInt[i] = Integer.valueOf(time[i]);
+							}
+
+							dueDate = LocalDate.of(timeInt[0], timeInt[1], timeInt[2]);
+							list.createNewTask(taskName, taskDescription, dueDate, taskTag);
+							
+							if (isFinished.equals("true")) {
+								list.getTaskByTaskName(taskName).setFinished(true);
+							}
+						}
 					}
-					
-					dueDate = LocalDate.of(timeInt[0], timeInt[1], timeInt[2]);
-					list.createNewTask(taskName, taskDes, dueDate, taskTag);
 				}
 			} catch (IOException e) {
 
@@ -110,5 +131,5 @@ public class ToDoListManager {
 			}
 		}
 	}
-	
+
 }
